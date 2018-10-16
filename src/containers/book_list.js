5bfaaab206e847512124baf07b5796bb95edb88e
Juan Carlos Estrada <jestrada@celerik.com>
2018-10-16 16:37:16 -0500

    sdfdsfsdf

---
+++ src/containers/book_list.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions';
+
+class BookList extends Component {
+
+    renderList () {
+        return this.props.books.map(book => {
+            return (
+                <li key={book.title} className="list-group-item">
+                    {book.title}
+                </li>
+            );
+        });
+    }
+
+    render () {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        );
+    }
+
+}
+
+function mapStateToProps (state) {
+    // Whatever is returned will show up as props
+    // inside of BookList
+    return {
+        books: state.books
+    };
+}
+
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps (dispatch) {
+    // Whenever selectBook is called, result should be passed
+    // to all of our reducers
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import booksReducer from './books-reducer';
+import BooksReducer from './books-reducer';
 
 const rootReducer = combineReducers({
-    books: booksReducer
+    books: BooksReducer
 });
 
 export default rootReducer;
